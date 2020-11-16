4e26bda4163e1eae03ca587f083d67986f4d7e91
Sergiy Naumov <1414sergiy@gmail.com>
2020-11-16 21:39:18 +0200

    Third commit

--- greeting.py
+++ greeting.py
@@ -1,7 +1,8 @@
-def greeting(name):
-    print(f'Hi, {name}! How are you?')
+def greeting(name, surname):
+    print(f'Hi, {name} {surname}! How are you?')
 
 
 if __name__ == '__main__':
     the_name = str(input('Please, enter your name: '))
-    greeting(the_name)
+    the_surname = str(input('Please, enter your surname: '))
+    greeting(the_name, the_surname)
