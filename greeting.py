e5f5af6a70f5576dcda3a7ef6dfc2e913f5b7ca5
Sergiy Naumov <1414sergiy@gmail.com>
2020-11-17 09:53:47 +0200

    First to develop

--- greeting.py
+++ greeting.py
@@ -1,8 +1,10 @@
-def greeting(name, surname):
+def greeting(name, surname, age):
     print(f'Hi, {name} {surname}! How are you?')
+    print(f"Are you only {age} years old?")
 
 
 if __name__ == '__main__':
     the_name = str(input('Please, enter your name: '))
     the_surname = str(input('Please, enter your surname: '))
-    greeting(the_name, the_surname)
+    the_age = int(input('How old are you?: '))
+    greeting(the_name, the_surname, the_age)
